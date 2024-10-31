f62c81ca50283bf9763fa2e1c20f195b557bf2a6
“zwliu2” <youfengyiyang@163.com>
2024-10-31 18:06:57 +0800

    Partially commit changes to mstringutils.go

--- mstringutils.go
+++ mstringutils.go
@@ -5,13 +5,19 @@ import (
 )
 
 // St is a custom string type.
-type St string
+type St struct {
+	value string
+}
+
+// NewSt creates a new St instance with an optional initial value.
+func NewSt(value ...string) *St {
+	if len(value) > 0 {
+		return &St{value: value[0]}
+	}
+	return &St{value: ""}
+}
 
 // ToUpperCase converts the given string to upper case.
-// Example usage:
-// s := St("hello")
-// result := s.ToUpperCase("world")
-// fmt.Println(result) // Output: WORLD
-func (s St) ToUpperCase(s1 string) string {
+func (s *St) ToUpperCase(s1 string) string {
 	return strings.ToUpper(s1)
 }
